c2cac4eb6eec0a47090585403422d64c2d86bcee
ZeroGodForce <yo@bucky.io>
2015-03-02 21:21:35 -0500

    Works but does not yet update the item quantity via add to cart button

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,164 @@
 (function() {
 	var app = angular.module('refStore', []);
 	
-	app.controller('StoreController', function(){
+	app.controller('StoreController', function($scope){
 		this.products = items;
+		
+		$scope.invoice = {
+			items: [{
+				name: 'Slime Blower',
+				qty: 2,
+				price: 2.95,
+				image:'http://placehold.it/250x100'
+			}]
+		};	
+	
+	
+	$scope.addProduct = function() {
+        console.log("this contains " + this.product.name);
+    };
+		
+		$scope.addItem = function(product) {
+        console.log("you have added " + this.product.name);
+			
+		
 
+/* 		angular.forEach($scope.invoice.items, function(item){   
+			for(var i = 0; i < $scope.invoice.items.length; i++){
+				console.log($scope.invoice.items[i]);
+				if(item.name.search($scope.invoice.items[i]) >= 0) {
+
+					console.log("found a match!");
+				}
+			}
+		}); */
+
+
+
+			$scope.invoice.items.push({
+				name: this.product.name,
+				qty: 1,
+				price: this.product.price
+			});
+		};
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	});
 	
- 	app.controller('CartController', function(){
+	
+	
+
+/*  	app.controller('CartController', function(){
 		this.cart = {};
 		
 		this.addToCart = function(product) {
-			
+			this.cart.push(this.product);
 		};
-	});
+	}); */
+	
+	
+/* 	app.controller('StoreController', ['$scope', function($scope) {
+				this.products = items;
+
+		$scope.invoice = { 
+			items: [{}]    
+		};
+
+  $scope.addToCart = function(product) {
+	  console.log(this.product);
+    this.product = $scope.orderline;
+  };
+}]);
+	 */
+	
+	
+	
+	
+	
+	
+	app.controller('CartController', function($scope){
+    
+
+    
+
+    $scope.removeItem = function(index) {
+        $scope.invoice.items.splice(index, 1);
+    };
+
+    $scope.total = function() {
+        var total = 0;
+        angular.forEach($scope.invoice.items, function(item) {
+            total += item.qty * item.price;
+        });
+
+        return total;
+    };
+});
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 var items = [
 		{
 			name: 'Proton Pack',
 			price: 2.95,
-			description: '...',
 			image:'http://placehold.it/250x100'
 		},
 		{
 			name: 'PKE Meter',
 			price: 5.95,
-			description: '...',
 			image:'http://placehold.it/250x100'
 		},
 	{
 			name: 'Ghost Trap',
 			price: 2.95,
-			description: '...',
 			image:'http://placehold.it/250x100'
 		},
 		{
 			name: 'Ecto Goggles',
 			price: 5.95,
-			description: '...',
 			image:'http://placehold.it/250x100'
 		},
 	{
 			name: 'Slime Blower',
 			price: 2.95,
-			description: '...',
 			image:'http://placehold.it/250x100'
 		}
 	];
+	
+	
+
+/* 		angular.module('scopeExample', []);
+app.controller('MyController', ['$scope', function($scope) {
+  $scope.username = 'World';
+
+  $scope.sayHello = function() {
+    $scope.greeting = 'Hello ' + $scope.username + '!';
+  };
+}]); */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 
 })();

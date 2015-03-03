f729b917fb4f2a5cb1f19d21aaff45078ef19600
ZeroGodForce <yo@bucky.io>
2015-03-03 16:45:45 -0500

    may have got it, need to tidy up the code and move the assignment push

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,18 @@
 	app.controller('StoreController', function($scope){
 		this.products = items;
 		
-		$scope.invoice = {
+ 		$scope.invoice = {
 			items: [{
 				name: 'Slime Blower',
 				qty: 2,
 				price: 2.95,
 				image:'http://placehold.it/250x100'
 			}]
-		};	
-	
+		};	 
+		
+/* 		$scope.invoice = {
+			items: [{}]
+		};	 */
 	
 		
 		$scope.addItem = function(product) {
@@ -22,14 +25,50 @@
 	angular.forEach($scope.invoice.items, function(item){   
 			for(var i = 0; i < $scope.invoice.items.length; i++){
 				
- 				console.log("the item is: " + $scope.item.name); 
+/*  				console.log("the item is: " + item.name);  */
 /* 				console.dir(item.name); */
 				
- 				console.log("the current index is at: " + $scope.invoice.items[i].name); 
-				if(item.name == $scope.invoice.items[i].name) {
+/*  				console.log("the current index is at: " + $scope.invoice.items[i].name);  */
+/* 				if(item.name == $scope.invoice.items[i].name) {
 
 					console.log("found a match!");
-				} 
+				}  */
+				
+/* 				console.log(isEquivalent($scope.invoice.items[i].name, item.name)); */
+				
+				
+ 					if(isEquivalent($scope.invoice.items[i].name, item.name)){
+						console.log("TRUE - item.name is: " + item.name);
+						console.log("TRUE - $scope.invoice.items[" + i + "].name is: " + $scope.invoice.items[i].name);
+					}else{
+ 						console.log("FALSE - item.name is: " + item.name);
+						console.log("FALSE - $scope.invoice.items[" + i + "].name is: " + $scope.invoice.items[i].name);
+					}
+				
+/* 				var prop = item;
+				var invoice = + $scope.invoice.items[i].name; */
+				
+/* 				for(prop in invoice) {
+					if(invoice.hasOwnProperty(prop)) {
+						if(invoice[prop] === "myValue") {
+							//Found myValue!
+						}
+					}
+				} */
+				
+/* 				for (var key in obj) {
+				  console.log("o." + key + " = " + obj[key]);
+				} */
+				
+				
+
+/* 				for (var i=0; i < item.length; i++) {
+					if (myArray[i].name === nameKey) {
+						return myArray[i];
+					}
+				} */
+
+
 			}
 		});
 
@@ -98,6 +137,33 @@ var items = [
 		}
 	];
 	
+	
+	
+	function isEquivalent(a, b) {
+    // Create arrays of property names
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
+}
 	
 
 })();
